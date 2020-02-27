992ed7040ac66e2120b7cb5ed33258f0f34b50f4
Hendi Santika <hendisantika@gmail.com>
2020-02-28 06:41:35 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springboothikaricpsample/SpringbootHikaricpSampleApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springboothikaricpsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHikaricpSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootHikaricpSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springboothikaricpsample/SpringbootHikaricpSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springboothikaricpsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootHikaricpSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
