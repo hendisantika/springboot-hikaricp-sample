5cb73b23573bba88885de5151a9e1fcfed921ebf
Hendi Santika <hendisantika@gmail.com>
2020-02-28 07:10:18 +0700

    #14 - Add Init in Main App

--- src/main/java/com/hendisantika/springboothikaricpsample/SpringbootHikaricpSampleApplication.java
+++ src/main/java/com/hendisantika/springboothikaricpsample/SpringbootHikaricpSampleApplication.java
@@ -1,10 +1,23 @@
 package com.hendisantika.springboothikaricpsample;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.sql.DataSource;
+
+@Slf4j
 @SpringBootApplication
-public class SpringbootHikaricpSampleApplication {
+public class SpringbootHikaricpSampleApplication implements CommandLineRunner {
+    @Autowired
+    DataSource dataSource;
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("DataSource = " + dataSource);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootHikaricpSampleApplication.class, args);
